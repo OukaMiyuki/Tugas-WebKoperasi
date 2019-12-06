3eb576cb0ee57f198f6a15d3c96f3ed3e88f9482
OukaMiyuki <asukaloid.blog@gmail.com>
2019-12-07 02:13:24 +0700

    Modul User

--- app/Http/Controllers/dataMember.php
+++ app/Http/Controllers/dataMember.php
@@ -11,24 +11,14 @@ class dataMember extends Controller
 {
     public function showMember(){
     	$users = DB::table('tb_anggotas')->count();
-    	//$data = DB::table('tb_anggotas')->get();
     	$data = DB::table('tb_anggotas')
     		->join('cek_ids', 'cek_ids.kode_member', '=', 'tb_anggotas.kode_member')
             ->join('usr_image_dbs', 'usr_image_dbs.kode_member', '=', 'tb_anggotas.kode_member')
     		->get();
     	$kode="";
         $cek = null;
-    	//echo $users;
-    	//if($users == 0){
-            $statement = DB::select("SHOW TABLE STATUS LIKE 'cek_ids'");
-            $cek = $statement[0]->Auto_increment;
-            //$cek = DB::table('cek_ids')->max('id') + 1;
-    	//	$kode = "USR".date("Ymd")."-1";
-    	//} else{
-    		//$cek = DB::table('cek_ids')->where(max('id'))->get();
-    	//	$cek = cek_id::max('id')+1;
-    		//$add = $users+1;
-    	//}
+        $statement = DB::select("SHOW TABLE STATUS LIKE 'cek_ids'");
+        $cek = $statement[0]->Auto_increment;
         $kode= "USR".date("Ymd")."-".$cek;
     	return view('index',['kode' => $kode, 'data' => $data]);
     }
